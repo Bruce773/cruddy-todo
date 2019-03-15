bd9efbb2a8ee38cf61593559e3a295f17914e1e6
Bruce <bruce@yogainternational.com>
2019-03-14 22:08:27 -0400

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -92,14 +92,29 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.readdir(exports.dataDir, (err, fileNamesArr) => {
+    if (err) {
+      console.log(err);
+    } else {
+      fileNamesArr.forEach((item, index) => {
+        var fileId = item.slice(0, item.length - 4);
+        if (fileId === id) {
+          fs.unlink(`${exports.dataDir}/${id}.txt`, (err) => {
+            if (err) {
+              // console.log(err);
+              callback(new Error(`No item with id: ${id}`));
+            } else {
+              callback();
+            }
+          });
+        } else if (fileId !== id && index === fileNamesArr.length - 1) {
+          callback(new Error(`No item with id: ${id}`));
+        }
+      });
+      // callback(null, { id, text });
+      // callback(null, data);
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
